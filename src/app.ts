a63a174107bd7463471af3750968e812c45de541
pankaj485 <pankajsamar2@gmail.com>
2024-02-07 08:16:27 +0530

    clean imports on server file

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+const express = require("express");
+import { Express, NextFunction, Request, Response } from "express";
+
+const app: Express = express();
+app.use(express.json());
+const PORT = 3000;
+
+type GlobalCatchMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void;
+
+const globalCatch: GlobalCatchMiddleware = (error, req, res, next) => {
+  res.status(500).json({
+    success: true,
+    message: "Internal server error",
+  });
+};
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({
+    success: true,
+    message: "Welcome to Express server",
+  });
+});
+
+// // global catch
+app.use(globalCatch);
+
+app.listen(PORT, () => {
+  console.log(`Express server is listening on PORT ${PORT}`);
+});
